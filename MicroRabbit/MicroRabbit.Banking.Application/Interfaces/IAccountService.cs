233a3a213b8905a438a63bcb95f8aa6e8d56e2af
canberk ustaoglu <canusta20@gmail.com>
2024-07-30 10:29:14 +0300

    Implemented Publish and SendCommand for Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -6,11 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Models;
 
 namespace MicroRabbit.Banking.Application.Interfaces
 {
     public interface IAccountService
     {
         IEnumerable<Account> GetAccounts();
+        void Transfer(AccountTransfer accountTransfer);
     }
 }
