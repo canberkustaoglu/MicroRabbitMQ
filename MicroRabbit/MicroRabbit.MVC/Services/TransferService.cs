f2cc9eca06503141f3112ebe0e2254b03d29582f
canberk ustaoglu <canusta20@gmail.com>
2024-08-06 11:53:57 +0300

    Project completed. Finito

---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:7194/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto),
+                System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
